7586a8a37ee00d53813b44581ce8268238fd31bd
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-28 02:09:08 +0300

    Adds more dependencies to the container

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,16 +6,14 @@ require ROOT_DIR . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
 
 $request = Request::createFromGlobals();
 
 $routes = include(ROOT_DIR . '/src/Routes.php');
+$container = include(ROOT_DIR . '/src/container.php');
 
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
+$container->get('context')->fromRequest($request);
+$matcher = $container->get('matcher');
 
 $response = new Response("Homepage");
 $response->send();
