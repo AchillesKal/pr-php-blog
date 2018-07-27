c0b7daa16c5b51c6f04c28990ff1bb6092a5bb1b
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-28 01:56:20 +0300

    Adds some dependencies to the container

--- src/container.php
+++ src/container.php
@@ -1,7 +1,19 @@
 <?php
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 $containerBuilder = new ContainerBuilder();
+
+$containerBuilder->register('context', RequestContext::class);
+
+$containerBuilder->register('matcher', UrlMatcher::class)
+    ->setArguments(array($routes, new Reference('context')));
+
+$containerBuilder->register('request_stack', RequestStack::class);
+
 
 return $containerBuilder;
