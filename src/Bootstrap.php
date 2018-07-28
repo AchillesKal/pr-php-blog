a3bf1419a6ed48b15fc6b2589a7796b25ab0cd3c
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-28 06:51:54 +0300

    removes dump

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,6 +6,7 @@ require ROOT_DIR . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
 
@@ -15,5 +16,17 @@ $container = include(ROOT_DIR . '/src/container.php');
 $container->get('context')->fromRequest($request);
 $matcher = $container->get('matcher');
 
-$response = new Response("Homepage");
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+
+    $controller =  $container->get('controller_resolver')->getController($request);
+    $arguments = $container->get('argument_resolver')->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
+} catch (ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
+}
+
 $response->send();
--- src/FrontPage/Presentation/FrontPageController.php
+++ src/FrontPage/Presentation/FrontPageController.php
@@ -2,10 +2,12 @@
 
 namespace PrPhpBlog\FrontPage\Presentation;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class FrontPageController
 {
     public function index()
     {
-
+        return new Response("Homepage");
     }
 }
