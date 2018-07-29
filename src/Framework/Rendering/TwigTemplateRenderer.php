b58751cc15131a181f74bae4767d6701d278cedc
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-07-30 01:00:01 +0300

    Adds Twig renderer

---
+++ src/Framework/Rendering/TwigTemplateRenderer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PrPhpBlog\Framework\Rendering;
+
+use Twig_Environment;
+
+class TwigTemplateRenderer implements TemplateRenderer
+{
+    private $twigEnvironment;
+
+    public function __construct(Twig_Environment $twigEnvironment)
+    {
+        $this->twigEnvironment = $twigEnvironment;
+    }
+
+    public function render(string $template, array $data = []): string
+    {
+        return $this->twigEnvironment->render($template, $data);
+    }
+}
