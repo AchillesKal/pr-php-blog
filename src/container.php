a12edb6f76c6841e0d6f553c373c9df535ca59a1
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-08-04 15:22:24 +0300

    Moves all the service definition process to services yaml file

--- src/container.php
+++ src/container.php
@@ -1,34 +1,13 @@
 <?php
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-use PrPHPBlog\Framework\Rendering\TwigTemplateRendererFactory;
-
-$containerBuilder = new ContainerBuilder();
-
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/src'));
+$container = new ContainerBuilder();
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__));
 $loader->load('services.yml');
+$container->setParameter('routes', $routes);
 
-$containerBuilder->register('context', RequestContext::class);
-
-$containerBuilder->register('matcher', UrlMatcher::class)
-    ->setArguments(array($routes, new Reference('context')));
-
-$containerBuilder->register('request_stack', RequestStack::class);
-
-$containerBuilder->register('controller_resolver', ControllerResolver::class);
-$containerBuilder->register('argument_resolver', ArgumentResolver::class);
-
-$containerBuilder->register('twig', TwigTemplateRendererFactory::class)
-    ->addMethodCall('create');
-
-return $containerBuilder;
+return $container;
